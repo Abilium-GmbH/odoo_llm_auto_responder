f9d13419b465f960793eefa9bea4356726953e9e
Gribi, Andrea Romeo (STUDENTS) <andrea.gribi@students.unibe.ch>
2024-03-17 18:50:13 +0100

    added flaskserver.py file

---
+++ addons/odoo_llm_auto_responder/flaskServer.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/ping', methods=['GET', 'POST']) # test
+def ping_pong():
+    # You can return a simple string
+    # return "pong"
+
+    # Or, for a more API-like response, return a JSON
+    return jsonify({"message": "pong"}), 200
