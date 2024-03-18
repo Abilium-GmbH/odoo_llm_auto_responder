0b7e57f575189afb09d26b18733a44c7554fbc07
Simon Egli <simon.egli4@students.unibe.ch>
2024-03-18 14:02:01 +0100

    Commmit flaskserver über Docker

---
+++ flask_server/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+#we define the route /
+@app.route('/')
+def welcome():
+    # return a json
+    return jsonify({'status': 'api working'})
+
+
+if __name__ == '__main__':
+    #define the localhost ip and the port that is going to be used
+    # in some future article, we are going to use an env variable instead a hardcoded port
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
