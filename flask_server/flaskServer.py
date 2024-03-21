a0a129f9f8b32295fc5176c0a7abfb70b497fbb3
Simon Egli <simon.egli4@students.unibe.ch>
2024-03-21 10:50:29 +0100

    Edit code and added comments and description

--- flask_server/flaskServer.py
+++ flask_server/flaskServer.py
@@ -1,5 +1,5 @@
 #this file runs a test-flaskserver
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route('/data', methods=['GET','POST'])
+def receive_data():
+    # Assuming the incoming data is JSON
+    data = request.get_json()
+    # You can process the data here and then respond
+    return jsonify({"message": "Data received", "yourData": data}), 200
 
 
 @app.route('/ping', methods=['GET', 'POST'])
