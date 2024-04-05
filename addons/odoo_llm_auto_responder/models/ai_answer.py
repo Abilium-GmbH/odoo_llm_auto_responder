2c68498a07f139bef019542caf8069f686526935
Fabian <fabian.schwarz@students.unibe.ch>
2024-04-05 17:14:15 +0200

    Tries to implement DB queries, not working yet

--- addons/odoo_llm_auto_responder/models/ai_answer.py
+++ addons/odoo_llm_auto_responder/models/ai_answer.py
@@ -1,6 +1,16 @@
 from odoo import fields, models
+import psycopg2
 import logging
 _logger = logging.getLogger(__name__)
+
+# Connect to an existing database
+""" conn = psycopg2.connect(
+    database='postgres',
+    user='odoo',
+    password='odoo',
+    host='0.0.0.0',
+    port='5432') """
+
 
 
 
@@ -10,7 +20,28 @@ class HelpdeskTicket(models.Model):
     # Adds a new field to the existing table of the ticket. There will be stored the generated answers of the AI
     ai_answer = fields.Text(string="AI Answer")
 
+
     # This method gets called when button is clicked -> Insert the DB queries here
     def ai_answer_button(self):
-        _logger.info("Test button clicked..")
+        _logger.info("Button was clicked...")
+
+    # Version 1
+        """self.env.cr.execute("insert into helpdesk.ticket (ai_answer) values ('Das ist eine vorgefertigte Antwort!')")
+        self.env.cr.commit()
+
+        res = self.env.cr.fetchall("select description from helpdesk.ticket")
+        logger.info(res)
+
+    # Version 2
+         # Open cursor to perform database operations
+        cur = conn.cursor()
+    # Insert data
+        cur.execute("INSERT INTO helpdesk.ticket (ai_answer) VALUES ('Das ist eine vorgefertigte Antwort!')")
+    # Query the database
+        cur.execute("SELECT description FROM helpdesk.ticket")
+        descriptions = cur.fetchall() 
+        logger.info(descriptions)"""
+
+    # Close communication with database
+    # cur.close()
 
