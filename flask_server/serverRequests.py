0b97ea408b8a4933957e7015f12abc1116fca28c
Simon Egli <simon.egli4@students.unibe.ch>
2024-04-08 21:52:28 +0200

    commiting other changes

--- flask_server/serverRequests.py
+++ flask_server/serverRequests.py
@@ -13,9 +13,10 @@ def send_data():
     url = 'http://localhost:5000/data'
     data = {'key': 'value', 'number': 123}
     response = requests.post(url, json=data)
+    print("Response status code:", response.status_code)
     print("Server responded with:", response.text)
 
+#curl -X POST -H "Content-Type: application/json" \-d "{\"context\":\"Here is some context about the question.\", \"question\":\"What is the question?\"}" \http://localhost:5000/llm
 
 if __name__ == "__main__":
-    ping_server()
     send_data()
