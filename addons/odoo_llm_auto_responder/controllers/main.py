be30aa36dbe0a5a7f033bc413e5f72e96814d87c
Fabian <fabian.schwarz@students.unibe.ch>
2024-04-15 23:30:14 +0200

    Changed code draft for meeting with Zühlke

--- addons/odoo_llm_auto_responder/controllers/main.py
+++ addons/odoo_llm_auto_responder/controllers/main.py
@@ -3,12 +3,11 @@ from odoo import http
 """class AIAnswerController(http.Controller):
     @http.route("/ai_anwser/answer", type="json", auth="user")
     def receive_ai_answer(self, **kw):
-    
-    Hier muss wahrscheinlich die ID aus dem JSON extrahiert werden
-     und dann im richtigen Ticket der Eintrag gemacht werden
-    id = kw.get("id")
-    self.ai_answer = kw.get("ai_answer")
-    self.
+       answer = http.request.env['helpdesk.ticket'].search([('id', '=', kw['id])])
+       answer.write('ai_answer': kw['ai_answer'])
+       return kw
+        
+   
     Meldung ausgeben, dass die Antwort für Ticketnummer angekommen ist
     und gespeichert wurde -> Popup"""
 
