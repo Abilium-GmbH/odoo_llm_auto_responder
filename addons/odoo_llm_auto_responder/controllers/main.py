7abecdb52e78a01617b755dc3341110834ce3513
Fabian <fabian.schwarz@students.unibe.ch>
2024-04-19 18:02:13 +0200

    Working AI answer controller

--- addons/odoo_llm_auto_responder/controllers/main.py
+++ addons/odoo_llm_auto_responder/controllers/main.py
@@ -7,8 +7,9 @@ class AIAnswerController(http.Controller):
     @http.route("/ai_answer/answer", type="json", auth="public")
     def receive_ai_answer(self, **kw):
         _logger.info("notification: received answer")
-        answer = http.request.env['helpdesk.ticket'].search([('id', '=', kw['id'])])
+        answer = http.request.env['helpdesk.ticket'].sudo().search([('id', '=', kw['id'])])
         answer.write({'ai_answer': kw['ai_answer']})
+        _logger.info("Description", answer.ai_answer)
         _logger.info("notification: saved answer")
         return 1
 
