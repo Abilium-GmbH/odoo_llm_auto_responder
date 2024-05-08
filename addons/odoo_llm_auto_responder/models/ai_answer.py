dfd795e9b03fee036470fee54fcd4901c64d96dc
Fabian <fabian.schwarz@students.unibe.ch>
2024-05-08 16:44:37 +0200

    Working automatic LLM answering

--- addons/odoo_llm_auto_responder/models/ai_answer.py
+++ addons/odoo_llm_auto_responder/models/ai_answer.py
@@ -12,8 +12,7 @@ class HelpdeskTicket(models.Model):
     # adds a new field to the existing table of the ticket. There will be stored the generated answers of the AI.
     ai_answer = fields.Text(string="AI Answer")
     ai_answer_ready = fields.Boolean(string="AI Answer is ready", default=False)
-
-    #answer_regenerated = fields.Text(string="regenerated Answer!")
+    description = fields.Text(inverse='_set_description')
 
     # this method gets called when button is clicked
 
@@ -51,6 +50,25 @@ class HelpdeskTicket(models.Model):
     def show_ai_answer(self):
         _logger.info("showed answer...")
         return None
+
+    # this inverse method triggers automatic LLM answering
+    def _set_description(self):
+        _logger.info("inverse description triggered")
+
+        json_data = {
+            "qId": self.id,
+            "question": self.description,
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        url = 'http://app:5000/data'
+        try:
+            requests.post(url, json=json_data, headers=headers)
+        except:
+            _logger.info("LLM not available")
+            raise AccessError("LLM not available")
+        finally:
+            return
 
 
     """@api.onchange('ai_answer_ready')
